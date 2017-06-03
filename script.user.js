437f4c5ad976387b07cd34d8e5b952b8aa4718a9
leoglock <synop1337@gmail.com>
2017-06-03 22:38:41 +0200

    Add files via upload

---
+++ script.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Mytorrent.ovh :: Useful Script
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Functions: Auto displays upload box, Autofills folder input, Auto checks "autostart downloading" option, Modifies "stream" button url to FTP one's
+// @author       LeO
+// @match        http://mytorrent.ovh:9091/transmission/web/
+// @match        http://mytorrent.ovh
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+	// Don't forget to rename "YOUR_FOLDER". Thanks to ANGA for sharing his seedbox.
+    $('a[href="http://www.mytorrent.ovh/downloader/"]').attr('href','http://www.mytorrent.ovh/here/YOUR_FOLDER'); // Modifies stream url to FTP url (path folder)
+    document.getElementById('upload_container').style="display:block;"; // Displays the upload box automatically
+    document.getElementById('add-dialog-folder-input').value="/home/downloader/here/YOUR_FOLDER"; // Autofills the folder input with a desired name (example: /home/downloader/here/michael).
+    document.getElementById('torrent_auto_start').checked="checked"; // Automatically checks the "autostart downloading" option.
+})();
