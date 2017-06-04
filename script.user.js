2e5f27f15822661e0f8f84a01e6a263da0150509
leoglock <synop1337@gmail.com>
2017-06-05 01:38:04 +0200

    Adding function
    Added a function which filters downloading torrents.

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Transmission :: Useful Script
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.2
 // @description  Functions: Auto displays upload box, Autofills folder input, Auto checks "autostart downloading" option, Modifies "stream" button url to FTP one's
 // @author       LeO
 // @updateURL    https://github.com/leoglock/Seedbox_Script/blob/master/script.user.js
@@ -12,12 +12,13 @@
 (function() {
     'use strict';
 	// Don't forget to rename "YOUR_FOLDER".
-    document.getElementById('upload_container').style="display:block;"; // Displays the upload box automatically
-    document.getElementById('add-dialog-folder-input').value="/home/downloader/here/YOUR_FOLDER/"; // Autofills the folder input with a desired name (example: /home/downloader/here/michael).
-    document.getElementById('torrent_auto_start').checked="checked"; // Automatically checks the "autostart downloading" option.
+    $('select>option:eq(2)').attr('selected', 'selected').trigger('change'); // Automatically filtering downloading torrents so that you can easily find your downloading torrent(s) in the list
+    document.getElementById('upload_container').style="display:block;"; // Automatically displaying the upload box.
+    document.getElementById('add-dialog-folder-input').value="/home/downloader/here/YOUR_FOLDER/"; // Automatically auto-filling the folder input with a desired name (example: /home/downloader/here/michael).
+    document.getElementById('torrent_auto_start').checked="checked"; //  Automatically checking the "autostart downloading" option automatically.
     $(document).ready(function(){
     $('a').each(function(){
-        this.href = this.href.replace('/downloader/', '/here/YOUR_FOLDER/'); // Modifies "stream" button url to FTP one's (with desired folder)
+        this.href = this.href.replace('/downloader/', '/here/YOUR_FOLDER/'); // Modifying "stream" button url to FTP one's (with desired folder)
     });
 });
 })();
