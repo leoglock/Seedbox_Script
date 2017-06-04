dc58cd6f099d7fa421b81d252f544ff30bd318c8
leoglock <synop1337@gmail.com>
2017-06-05 00:45:51 +0200

    Improved code
    I improved code to make the script working for everyone using transmission.

--- script.user.js
+++ script.user.js
@@ -1,20 +1,23 @@
 // ==UserScript==
-// @name         Mytorrent.ovh :: Useful Script
+// @name         Transmission :: Useful Script
 // @namespace    http://tampermonkey.net/
 // @version      0.2
 // @description  Functions: Auto displays upload box, Autofills folder input, Auto checks "autostart downloading" option, Modifies "stream" button url to FTP one's
 // @author       LeO
 // @updateURL    https://github.com/leoglock/Seedbox_Script/blob/master/script.user.js
-// @match        http://mytorrent.ovh:9091/transmission/web/
-// @match        http://mytorrent.ovh
+// @match        http://*/transmission/web/
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-	// Don't forget to rename "YOUR_FOLDER". Thanks to ANGA for sharing his seedbox.
-    $('a[href="http://www.mytorrent.ovh/downloader/"]').attr('href','http://www.mytorrent.ovh/here/YOUR_FOLDER'); // Modifies stream url to FTP url (path folder)
+	// Don't forget to rename "YOUR_FOLDER".
     document.getElementById('upload_container').style="display:block;"; // Displays the upload box automatically
-    document.getElementById('add-dialog-folder-input').value="/home/downloader/here/YOUR_FOLDER"; // Autofills the folder input with a desired name (example: /home/downloader/here/michael).
+    document.getElementById('add-dialog-folder-input').value="/home/downloader/here/leo/"; // Autofills the folder input with a desired name (example: /home/downloader/here/michael).
     document.getElementById('torrent_auto_start').checked="checked"; // Automatically checks the "autostart downloading" option.
+    $(document).ready(function(){
+    $('a').each(function(){
+        this.href = this.href.replace('/downloader/', '/here/YOUR_FOLDER/'); // Modifies "stream" button url to FTP one's (with desired folder)
+    });
+});
 })();
